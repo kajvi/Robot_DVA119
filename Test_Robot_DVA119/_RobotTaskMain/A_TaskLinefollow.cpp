20237f7a5a60e11e5ae893f23cde08fce84dcf16
kajvi <daniel.stenekap@gmail.com>
2015-10-06 17:26:05 +0200

    Setup State machine (i hope)
    Sensor data gives actions suggestions, state updates depending on how
    persistant a suggestion is.
    3 lvls for Turns and for Finnished (exit task).
    2 lvls for Forward and Backward.

--- Test_Robot_DVA119/_RobotTaskMain/A_TaskLinefollow.cpp
+++ Test_Robot_DVA119/_RobotTaskMain/A_TaskLinefollow.cpp
@@ -9,16 +9,22 @@
 enum robotStateEnum {
   rsUnknown,
   rsInitial,
-  rsStraight,
-  rsLeft,
-  rsSharpLeft,
-  rsRight,
-  rsSharpRight,
-  rsBackward,
-  rsFinnished
+  rsStraight1,
+  rsStraight2,
+  rsLeft1,
+  rsLeft2,
+  rsLeft3,
+  rsRight1,
+  rsRight2,
+  rsRight3,
+  rsBackward1,
+  rsBackward2,
+  rsFinnished1
+  rsFinnished2
+  rsFinnished3
 };
 
-enum lastestDirectionTurn {
+enum lastestDirectionEnum {
   ldUnknown,
   ldInitial,
   ldStraight,
@@ -26,8 +32,21 @@ enum lastestDirectionTurn {
   ldRight
 };
 
+// Sets action of the robot depending on the state of the robot.
+enum actionEnum {
+  aeUnknown,
+  aeInitial,
+  aeStill,
+  aeLeft,
+  aeRight,
+  aeForward,
+  aeBackward,
+  aeFinnished
+};
+
 static enum robotStateEnum stat_RobotState = rsInitial;
-enum lastestDirectionTurn stat_LastestDirection = ldInitial;
+static enum lastestDirectionEnum stat_LastestDirection = ldInitial;
+static enum actionEnum stat_Action = aeInitial;
 
 // ============================================================================================
 
@@ -40,96 +59,173 @@ void taskLineFollow(struct ioStruct* ptr_io)
   // Evaluate sensor data
   // ====================
 
-  // black is found on middle and Left - turn a little left!
+// black is found on middle and Left - turn a little left!
   if ( (ptr_io->iosReflFrontLeft_0 == C_DARK_1) &&  (ptr_io->iosReflFrontCenter_1 == C_DARK_1) && (ptr_io->iosReflFrontRight_2 == C_LIGHT_0) )
   {
-    stat_RobotState = rsLeft;
+    stat_Action = aeLeft;
   } // if
 
 
   // black is found only on Left - turn sharp left!
   if ( (ptr_io->iosReflFrontLeft_0 == C_DARK_1) &&  (ptr_io->iosReflFrontCenter_1 == C_LIGHT_0) && (ptr_io->iosReflFrontRight_2 == C_LIGHT_0) )
   {
-    stat_RobotState = rsSharpLeft;
+    sstat_Action = aeLeft;
   } // if
 
 
   // black is found only on Middle and Right - turn half right!
   if( (ptr_io->iosReflFrontLeft_0 == C_LIGHT_0) &&  (ptr_io->iosReflFrontCenter_1 == C_DARK_1) && (ptr_io->iosReflFrontRight_2 == C_DARK_1) )
   {
-    stat_RobotState = rsRight;
+    stat_Action = aeRight;
   } // if
 
 
   // black is found only on Right - turn right!
   if( (ptr_io->iosReflFrontLeft_0 == C_LIGHT_0) &&  (ptr_io->iosReflFrontCenter_1 == C_LIGHT_0) && (ptr_io->iosReflFrontRight_2 == C_DARK_1) )
   {
-      stat_RobotState = rsSharpRight;
+    stat_Action = aeRight
   } // if
 
 
   // Black is found in middle - go forward!
   if ( (ptr_io->iosReflFrontLeft_0 == C_LIGHT_0) &&  (ptr_io->iosReflFrontCenter_1 == C_DARK_1) && (ptr_io->iosReflFrontRight_2 == C_LIGHT_0) )
   {
-    stat_RobotState = rsStraight;
+    stat_Action = aeStraight;
   } // if
 
 
   // No black is found - Back up alittle!
   if ( (ptr_io->iosReflFrontLeft_0 == C_LIGHT_0) &&  (ptr_io->iosReflFrontCenter_1 == C_LIGHT_0) && (ptr_io->iosReflFrontRight_2 == C_LIGHT_0) )
   {
-        stat_RobotState = rsBackward;
+    stat_Action = aeBackward;
   } // if
 
 
   // Black is found on all: Different actions depending on state and direction.
   if ( (ptr_io->iosReflFrontLeft_0 == C_DARK_1) &&  (ptr_io->iosReflFrontCenter_1 == C_DARK_1) && (ptr_io->iosReflFrontRight_2 == C_DARK_1) )
   {
-    // Black is found on all and previous state was forward - Task compleeted!
-    if (stat_RobotState == rsStraight)
-    {
-      stat_RobotState = rsFinnished;
-    }
-    
-    // Else depending on current state and the previous known direction:
-    else
-    {
-      // Keep going left but not sharply
-      if (stat_RobotState == rsSharpLeft)
-      {
-        stat_RobotState = rsLeft;
-      }
+    stat_Action = aeFinnished;
+  }
 
-      // Keep going right but not sharply
-      if (stat_RobotState == rsSharpRight)
-      {
-        stat_RobotState == rsRight;    
-      }
-
-      // State backward means we lost track of the line during a turn - depending on latest known direction of turn:
-      if (stat_RobotState == rsBackward)
-      {
-         // if we lost the line during a left turn, we now returned to the line and we make a sharp left (override with iosDelayMs).
-        if(stat_LastestDirection == ldLeft)
-        {
-          stat_RobotState == rsSharpLeft;
-        }
-
-        // if we lost the line during a Right turn, we now returned to the line and we make a sharp Right (override with iosDelayMs).
-        if(stat_LastestDirection == ldRight)
-        {
-          stat_RobotState == rsSharpRight;
-        }
-      }
-    }
-  } // i
-  
 
   if ( (ptr_io->iosReflFrontLeft_0 == C_DARK_1) &&  (ptr_io->iosReflFrontCenter_1 == C_LIGHT_0) && (ptr_io->iosReflFrontRight_2 == C_DARK_1) )
   {
     // Black is found on Left + Rigth but not on Middle - what to do?!
-    //actionMode = aeActionUnknown;
+    stat_Action = aeStraight;
   } // if
+
+  switch (stat_RobotState)
+  {
+    // Initial Value : Drive Forward
+    case rsInitial:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion straight "1 time"
+    case rsStraight1:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion straight "2 time"
+    case rsStraight2:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion straight "3 time"
+    case rsStraight3:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion Left "1 time"
+    case rsLeft1:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion Left "2 time"
+    case rsLeft2:
+    {
+      // TO DO!
+      break;
+    }
+    
+    // Action suggestion Left "3 time"
+    case rsLeft3:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion Right "1 time"
+    case rsRight1:
+    {
+      // TO DO!
+      break;
+    }
+    
+    // Action suggestion Right "2 time"
+    case rsRight2:
+    {
+      // TO DO!
+      break;
+    }
+    
+    // Action suggestion Right "3 time"
+    case rsRight3:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion Backward1 "1 time"
+    case rsBackward1:
+    {
+      // TO DO!
+      break;
+    }
+    
+    // Action suggestion Backward2 "2 time"
+    case rsBackward2:
+    {
+      // TO DO!
+      break;
+    }
+
+   // Action suggestion Finnished "1 time"
+    case rsFinnished:
+    {
+      // TO DO!
+      break;
+    }
+
+    // Action suggestion Finnished "2 time"
+    case rsFinnished2:
+    {
+      // TO DO!
+      break;
+    }
+    
+    // Action suggestion Finnished "3 time"
+    case rsFinnished3:
+    {
+      // TO DO!
+      break;
+    }
+    
+    default:
+    break;
+  }
+  
+
   
 
   
